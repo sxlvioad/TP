8cc38750f0f33aa38c2620730d982bb04b4ed724
milixb <mborroni@L2Maq04.W2008R2L2.local>
2017-10-23 11:15:35 -0300

    clases 0.3

--- src/App.java
+++ src/App.java
@@ -9,7 +9,7 @@ public class App {
 
 	public static void main(String[] args) {
 		JFrame marco = new JFrame();
-		
+
 		marco.setVisible(true);
 		marco.setBounds(0, 0, 500, 400);
 		marco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
